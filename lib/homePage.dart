4b596cbdf8473e7502b7d564f20bf12e4d086059
Bao Tran <bao.tran@sstechvn.com>
2022-07-22 17:44:29 +0700

    Implement HomePage body

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -6,6 +6,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+        backgroundColor: Colors.black87,
         appBar: AppBar(
           title: Text('Anime'),
           flexibleSpace: Container(
@@ -20,7 +21,8 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           imageSlider(context),
-          genresSlider(context)
+          genresSlider(context),
+          movieList(context)
         ],
       )
     );
@@ -50,15 +52,18 @@ Widget imageSlider(context) {
           child: Swiper(
             itemBuilder: (BuildContext context, int index) {
               return Card(
-                elevation: 8,
-                shadowColor: Colors.grey,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(3.0),
+                ),
+                elevation: 3,
+                shadowColor: Colors.white,
                 child: Image.asset(
                   imageList[index],
                   fit: BoxFit.fitHeight
                 ),
               );
             },
-            itemCount: 10,
+            itemCount: imageList.length,
             viewportFraction: 0.5,
             scale: 0.7,
             fade: 0.1
@@ -70,7 +75,7 @@ Widget imageSlider(context) {
 }
 
 Widget genresSlider(context) {
-  final genres = [
+  final List<String> genres = <String>[
     'Action',
     'Adventure',
     'Comedy',
@@ -104,7 +109,7 @@ Widget genresSlider(context) {
                 ),
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.grey.withOpacity(0.8),
+                    color: Colors.black45.withOpacity(0.8),
                     blurRadius: 5,
                     offset: Offset(5, 15),
                   ),
@@ -124,4 +129,270 @@ Widget genresSlider(context) {
       ),
     ),
   );
+}
+
+Widget movieList(context) {
+  final list = [
+    {
+      'title': 'mới cập nhật',
+      'movies': [
+        {
+          'name': 'Overlord',
+          'thumb': 'assets/images/overlord-ss4.jpeg'
+        },
+        {
+          'name': 'One Piece',
+          'thumb': 'assets/images/one-piece.jpeg'
+        },
+        {
+          'name': 'Naruto Shippuden',
+          'thumb': 'assets/images/naruto.jpeg'
+        },
+        {
+          'name': 'Bleach',
+          'thumb': 'assets/images/bleach.jpeg'
+        },
+        {
+          'name': 'My Hero Academia',
+          'thumb': 'assets/images/my-hero-academia.png'
+        },
+        {
+          'name': 'One Punch Man',
+          'thumb': 'assets/images/one-punch-man.png'
+        },
+        {
+          'name': 'Jujutsu Kaisen',
+          'thumb': 'assets/images/jujutsu-kaisen.jpeg'
+        },
+        {
+          'name': 'Kimetsu no Yaiba',
+          'thumb': 'assets/images/kimetsu-no-yaiba.jpeg'
+        },
+        {
+          'name': 'Fairy Tail',
+          'thumb': 'assets/images/fairy-tail.jpeg'
+        },
+        {
+          'name': 'Black Clover',
+          'thumb': 'assets/images/black-clover.jpeg'
+        },
+      ]
+    },
+    {
+      'title': 'sắp chiếu',
+      'movies': [
+        {
+          'name': 'Overlord',
+          'thumb': 'assets/images/overlord-ss4.jpeg'
+        },
+        {
+          'name': 'One Piece',
+          'thumb': 'assets/images/one-piece.jpeg'
+        },
+        {
+          'name': 'Naruto Shippuden',
+          'thumb': 'assets/images/naruto.jpeg'
+        },
+        {
+          'name': 'Bleach',
+          'thumb': 'assets/images/bleach.jpeg'
+        },
+        {
+          'name': 'My Hero Academia',
+          'thumb': 'assets/images/my-hero-academia.png'
+        },
+        {
+          'name': 'One Punch Man',
+          'thumb': 'assets/images/one-punch-man.png'
+        },
+        {
+          'name': 'Jujutsu Kaisen',
+          'thumb': 'assets/images/jujutsu-kaisen.jpeg'
+        },
+        {
+          'name': 'Kimetsu no Yaiba',
+          'thumb': 'assets/images/kimetsu-no-yaiba.jpeg'
+        },
+        {
+          'name': 'Fairy Tail',
+          'thumb': 'assets/images/fairy-tail.jpeg'
+        },
+        {
+          'name': 'Black Clover',
+          'thumb': 'assets/images/black-clover.jpeg'
+        },
+      ]
+    },
+    {
+      'title': 'đề cử',
+      'movies': [
+        {
+          'name': 'Overlord',
+          'thumb': 'assets/images/overlord-ss4.jpeg'
+        },
+        {
+          'name': 'One Piece',
+          'thumb': 'assets/images/one-piece.jpeg'
+        },
+        {
+          'name': 'Naruto Shippuden',
+          'thumb': 'assets/images/naruto.jpeg'
+        },
+        {
+          'name': 'Bleach',
+          'thumb': 'assets/images/bleach.jpeg'
+        },
+        {
+          'name': 'My Hero Academia',
+          'thumb': 'assets/images/my-hero-academia.png'
+        },
+        {
+          'name': 'One Punch Man',
+          'thumb': 'assets/images/one-punch-man.png'
+        },
+        {
+          'name': 'Jujutsu Kaisen',
+          'thumb': 'assets/images/jujutsu-kaisen.jpeg'
+        },
+        {
+          'name': 'Kimetsu no Yaiba',
+          'thumb': 'assets/images/kimetsu-no-yaiba.jpeg'
+        },
+        {
+          'name': 'Fairy Tail',
+          'thumb': 'assets/images/fairy-tail.jpeg'
+        },
+        {
+          'name': 'Black Clover',
+          'thumb': 'assets/images/black-clover.jpeg'
+        },
+      ]
+    },
+    {
+      'title': 'tin tức',
+      'movies': [
+        {
+          'name': 'Overlord',
+          'thumb': 'assets/images/overlord-ss4.jpeg'
+        },
+        {
+          'name': 'One Piece',
+          'thumb': 'assets/images/one-piece.jpeg'
+        },
+        {
+          'name': 'Naruto Shippuden',
+          'thumb': 'assets/images/naruto.jpeg'
+        },
+        {
+          'name': 'Bleach',
+          'thumb': 'assets/images/bleach.jpeg'
+        },
+        {
+          'name': 'My Hero Academia',
+          'thumb': 'assets/images/my-hero-academia.png'
+        },
+        {
+          'name': 'One Punch Man',
+          'thumb': 'assets/images/one-punch-man.png'
+        },
+        {
+          'name': 'Jujutsu Kaisen',
+          'thumb': 'assets/images/jujutsu-kaisen.jpeg'
+        },
+        {
+          'name': 'Kimetsu no Yaiba',
+          'thumb': 'assets/images/kimetsu-no-yaiba.jpeg'
+        },
+        {
+          'name': 'Fairy Tail',
+          'thumb': 'assets/images/fairy-tail.jpeg'
+        },
+        {
+          'name': 'Black Clover',
+          'thumb': 'assets/images/black-clover.jpeg'
+        },
+      ]
+    }
+  ];
+
+  return Container(
+    padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 10.0),
+    child: ListView.builder(
+      itemCount: list.length,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          color: Colors.black87,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(3.0),
+          ),
+          elevation: 3,
+          child: Padding(
+            padding: EdgeInsets.only(top: 10.0, bottom: 10.0, left: 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  list[index]['title'].toString().toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontFamily: 'MonteSerrat',
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: moviesMaker(list[index]['movies']),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ),
+  );
+}
+
+moviesMaker(list) {
+  final children = <Widget>[];
+  for (var i = 0; i < list.length; i++) {
+    children.add(
+      Container(
+        width: 150.0,
+        height: 200.0,
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: Image.asset(
+                  list[i]['thumb'],
+                  fit: BoxFit.fitHeight,
+                ),
+              ),
+            ),
+            Text(
+              list[i]['name'],
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
+        )
+      )
+    );
+  }
+  return children;
 }
